6e3d8b0ed06d529d2d04d8fd6b1fa7167f6e00e5
Osahon Irabor <osahonirabor@hotmail.com>
2023-02-22 01:40:46 -0800

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+    return ( 
+        <>
+        <Head>
+            <title>Ninja List | About</title> 
+            <meta name="keywords" content="ninjas" />
+        </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis consectetur cumque odit minima nesciunt accusamus aperiam quas magni repellendus error sint inventore, libero aliquam fugit repellat fugiat? Adipisci, harum a.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ad praesentium in tempora fuga, eius saepe repellat deserunt, eos unde sint omnis hic laboriosam, dolore odio cumque. Repellat, quam cupiditate.</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
